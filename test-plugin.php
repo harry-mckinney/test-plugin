dc9e7a1d4c56490e34bfaf55f1e745d5094b4856
Harry McKinney <hmckinney@hdiac.org>
2018-07-23 10:36:24 -0400

    update test plugin to prepare for initial deployment test
    Plugin was created to test git to wordpress rapid deployment and hooks

---
+++ test-plugin.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+*@package Test Plugin
+*/
+
+/*
+Plugin Name: Test Plugin
+Plugin URI: http://www.test@test.net
+Description: Test Plugin
+Version: 1.0.0
+Author: Test Plugin Engineering Society
+Author URI: http://test-land.test
+License: MIT
+Text Domain: test-Plugin
+*/
